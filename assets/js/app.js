a7623f9e573ae750d174001c5231402b8c98e39f
Paul Herman <pmherman85@icloud.com>
2020-06-25 14:52:30 -0400

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
         )};
     })
     
-    const RSS_URL = 'https://fullfrontalnerdityshow.libsyn.com/rss';
+    const RSS_URL = 'https://fullfrontalnerdityshow.libsyn.com/rss?include-libsyn-metadata=true';
 
     $.ajax(RSS_URL, {
         accepts: {
@@ -51,32 +51,53 @@ $(document).ready(function() {
         dataType: "xml",
       
         success: function(data) {
-            console.log(data)
+            const TOTAL = $(data).find('item').length;
+            const SITE = document.location.href.match(/[^\/]+$/)[0];
+            console.log(SITE);
           $(data)
             .find("item")
-            .each(function() {
+            .each(function(i) {
               const el = $(this);
-      
+                
               const template = `
-                <div class='col-xl-4 podcast'>
+                <div class='col-xl-10 podcast'>
                     <article>
-                        <h2>
+                        <h2> 
                             <a href="${el
                             .find("link")
                             .text()}" target="_blank" rel="noopener">
                             ${el.find("title").text()}
                             </a>
                         </h2>
+                        <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/${el.find('libsyn\\:itemId').text()}/height/90/theme/custom/thumbnail/yes/direction/forward/render-playlist/no/custom-color/b1b3ac/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
                         <h6>${el.find('pubDate').text()}</h6>
                         <p>
-                            ${el
-                                .find('description').text()}
+                            ${el.find('description').text()}
                         </p>
+                        <p></p>
                     </article>
                 </div>
               `;
-      
-              document.getElementById('podcasts').insertAdjacentHTML("beforeend", template);
+              if ( SITE === 'podcasts.html' && i < 14) {
+                console.log(i);
+                document.getElementById('podcasts').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_2.html' && i > 15 && i < 29 ) {
+                document.getElementById('podcasts2').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_3.html' && i > 30 && i < 44 ) {
+                document.getElementById('podcasts3').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_4.html' && i > 30 && i < 44 ) {
+                document.getElementById('podcasts4').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_5.html' && i > 45 && i < 59 ) {
+                document.getElementById('podcasts5').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_6.html' && i > 60 && i < 74 ) {
+                document.getElementById('podcasts6').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_7.html' && i > 75 && i < 89 ) {
+                document.getElementById('podcasts7').insertAdjacentHTML("beforeend", template);
+              } else if ( SITE === 'podcasts_8.html' && i > 90 ) {
+                  console.log(i)
+                document.getElementById('podcasts8').insertAdjacentHTML("beforeend", template);
+              } 
+
             });
         }
       });
